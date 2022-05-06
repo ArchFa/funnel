8aca2deea224fc0465b2e87bcef912d287e74acf
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-05-06 20:00:52 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -11,23 +11,30 @@ import time
 from datetime import datetime
 
 # %%
-d = st.date_input(
-     "When's your birthday",)
-st.write('Your birthday is:', d)
+start_day = st.date_input(
+     "Дата начала",)
+st.write('Воронка с', start_day, 'числа')
 
 # %%
-# while True:
-#     header={
-#         'Content-Type': 'application/x-yametrika+json',
-#         'Authorization': 'OAuth AQAEA7qjKZXKAAfhX_j1dIIUR059lNONgVpbkWk'
-#     }
+end_day = st.date_input(
+     "Дата конца",)
+st.write('Воронка до', end_day, 'числа')
 
-#     metrika_url = 'https://api.appmetrica.yandex.ru/logs/v1/export/events.json?application_id=4065523&date_since=2022-04-30%2000%3A00%3A00&date_until=2022-05-06%2023%3A59%3A59&date_dimension=default&use_utf8_bom=true&fields=appmetrica_device_id%2Ccity%2Cevent_name'
+# %%
+while True:
+    header={
+        'Content-Type': 'application/x-yametrika+json',
+        'Authorization': 'OAuth AQAEA7qjKZXKAAfhX_j1dIIUR059lNONgVpbkWk'
+    }
 
-#     res = r.get(metrika_url, headers=header)
-#     res
-#     if res.status_code == 200:
-#         break
-#     time.sleep(300)
+    metrika_url = f'https://api.appmetrica.yandex.ru/logs/v1/export/events.json?application_id=4065523&date_since={start_day}%2000%3A00%3A00&date_until={end_day}%2023%3A59%3A59&date_dimension=default&use_utf8_bom=true&fields=appmetrica_device_id%2Ccity%2Cevent_name'
+
+    res = r.get(metrika_url, headers=header)
+    if res.status_code == 200:
+        items = json.loads(res.text.lstrip('\ufeff'))
+        df = pd.DataFrame.from_dict(items['data'])
+        st.write(df)
+        break
+    time.sleep(300)
 
 
